18e580430403952ecf0de7fd0663aa59b7bdf937
ben <benjaminpla.dev@gmail.com>
2025-02-01 14:48:32 +0100

    add addPriorityFeesToTransaction exercise

---
+++ src/add_priority_fees_to_transaction.ts
@@ -0,0 +1,64 @@
+import * as web3 from "@solana/web3.js";
+import { getKeypairFromFile } from "@solana-developers/helpers";
+import {
+  createConnection,
+  handleError,
+  logBalance,
+  requestLamportsIfNeeded,
+} from "./utils";
+
+const addPriorityFeesToTransaction = async () => {
+  console.log("[addPriorityFee]: running...");
+  try {
+    const senderKeypair = await getKeypairFromFile("~/.config/solana/id.json");
+    const recipientKeypair = await getKeypairFromFile(
+      "~/.config/solana/keypair_2.json",
+    );
+
+    const connection = createConnection();
+
+    await logBalance(connection, senderKeypair.publicKey, "Sender");
+    await logBalance(connection, recipientKeypair.publicKey, "Recipient");
+
+    await requestLamportsIfNeeded(connection, senderKeypair.publicKey);
+
+    const transaction = new web3.Transaction();
+
+    const instructions = web3.SystemProgram.transfer({
+      fromPubkey: senderKeypair.publicKey,
+      toPubkey: recipientKeypair.publicKey,
+      lamports: web3.LAMPORTS_PER_SOL * 0.1,
+    });
+
+    const units = 1_000_000;
+    console.log(`[modifyComputeUnits]: ${units} units`);
+    const modifyComputeUnits = web3.ComputeBudgetProgram.setComputeUnitLimit({
+      units,
+    });
+
+    const microLamports = 1;
+    console.log(`[addPriorityFee]: ${microLamports} microLamports`);
+    const addPriorityFee = web3.ComputeBudgetProgram.setComputeUnitPrice({
+      microLamports,
+    });
+
+    transaction.add(instructions);
+    transaction.add(modifyComputeUnits);
+    transaction.add(addPriorityFee);
+
+    const signature = await web3.sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [senderKeypair],
+    );
+
+    console.log("[Transaction successful!]");
+    console.log(`[Signature:] ${signature}`);
+    await logBalance(connection, senderKeypair.publicKey, "Sender");
+    await logBalance(connection, recipientKeypair.publicKey, "Recipient");
+  } catch (error: unknown) {
+    handleError(error, true);
+  }
+};
+
+addPriorityFeesToTransaction();
